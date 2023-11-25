45463f4408859dffc1346c33fd9cf1acda858107
KimJunHo98 <dydgnscc6845@gmail.com>
2023-11-25 18:19:24 +0900

    231125 useSearchVideo.js 수정필요(검색결과 반환을 제대로 못함)

--- src/context/useSearchVideo.js
+++ src/context/useSearchVideo.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { debounce } from "lodash";
+// import { debounce } from "lodash";
 
 const useSearchVideo = () => {
     const [searchResult, setSearchResult] = useState([]);
@@ -10,26 +10,18 @@ const useSearchVideo = () => {
     const navigate = useNavigate();
 
     const API_KEY = process.env.REACT_APP_API_KEY;
-    const API_URL = "https://youtube.googleapis.com/youtube/v3/";
-    
+    const API_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=16&order=viewCount&regionCode=kr&pageToken=CBAQAA&type=video&q=`;
+
     useEffect(() => {
-        const searchResponse = `${API_URL}search?part=snippet&maxResults=16&order=viewCount&pageToken=CBAQAA&q=${encodeURIComponent(
-            searchText
-        )}&regionCode=kr&videoCaption=any&key=${API_KEY}`;
-
-        if (searchText.trim() === "") {
-            setSearchResult([]);
-
-            return;
-        }
-    
-        const fetchData = debounce(async () => {
+        const fetchData = async () => {
             try {
                 setLoading(true);
 
+                const searchResponse = `${API_URL}${encodeURIComponent(searchText)}&key=${API_KEY}`;
+
                 const result = await axios.get(searchResponse);
                 const status = result.status;
-    
+
                 if (status === 200) {
                     const videos = result.data.items;
                     setSearchResult(videos);
@@ -38,29 +30,26 @@ const useSearchVideo = () => {
                 }
             } catch (error) {
                 console.error(error);
-            }finally {
+            } finally {
                 setLoading(false);
             }
-        }, 300);
-    
+        };
+
         fetchData();
     }, [searchText]);
 
     const onChange = (e) => {
         setSearchText(e.target.value);
+        console.log(searchText);
     };
 
-    const filteredVideos = searchResult.filter((search) => {
-        if (!search || !search.snippet.title) {
-            return false;
-        }
+    const filteredVideos = searchResult.filter(
+        (search) =>
+            search.snippet.title.toLowerCase().includes(searchText.toLowerCase()) ||
+            search.snippet.channelTitle.toLowerCase().includes(searchText.toLowerCase())
+    );
 
-        const videoTitleLower =
-            search.snippet.title.toLowerCase() || search.snippet.channelTitle.toLowerCase() || search.snippet.description.toLowerCase();
-        const searchTextLower = searchText.toLowerCase();
-
-        return videoTitleLower.includes(searchTextLower);
-    });
+    // console.log(filteredVideos);
 
     const handleInputSubmit = (e) => {
         e.preventDefault();
@@ -72,8 +61,6 @@ const useSearchVideo = () => {
         } else {
             navigate("/");
         }
-
-        setSearchResult([]);
     };
 
     return { searchResult, onChange, filteredVideos, searchText, setSearchText, loading, handleInputSubmit };
